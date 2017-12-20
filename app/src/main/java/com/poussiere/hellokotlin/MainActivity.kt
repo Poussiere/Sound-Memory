a853942b6157df83f63b699739f6d44735001f1b
poussiere <von.linnasta@gmail.com>
2017-12-21 00:32:02 +0100

    added play song option

--- app/src/main/java/com/poussiere/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/poussiere/hellokotlin/MainActivity.kt
@@ -2,8 +2,13 @@ package com.poussiere.hellokotlin
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.support.annotation.MainThread
 import android.support.v7.app.AppCompatActivity
+import com.jakewharton.rxbinding.view.RxView
+import com.poussiere.hellokotlin.utils.Song
 import kotlinx.android.synthetic.main.activity_main.*
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 /**
  * Created by poussiere on 18/12/1
@@ -13,11 +18,21 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
         mainTextView.setText(R.string.main_text_view_content)
+
+
+        val song = Song(this);
+        RxView.clicks(songTextView).subscribe{
+        song.play()
+        }
+
+
+
     }
 
 
---
+++ app/src/main/java/com/poussiere/hellokotlin/utils/Song.kt
@@ -0,0 +1,28 @@
+package com.poussiere.hellokotlin.utils
+
+import android.content.Context
+import android.media.MediaPlayer
+import com.poussiere.hellokotlin.R
+
+class Song (val context : Context){
+
+    var mediaPlayer : MediaPlayer?=null;
+
+
+
+    fun play (){
+       if (mediaPlayer==null) {
+           mediaPlayer = MediaPlayer.create(context, R.raw.bateau)
+           mediaPlayer?.start()
+           mediaPlayer?.setOnCompletionListener(MediaPlayer.OnCompletionListener {
+               mediaPlayer?.stop()
+               mediaPlayer?.release()
+               mediaPlayer = null;
+           })
+
+       }
+    }
+
+
+}
+
