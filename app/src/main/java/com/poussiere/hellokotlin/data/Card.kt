c4bd1c0eefd64253ed50825c908ec44c82754bac
Poussiere <von.linnasta@gmail.com>
2018-01-25 11:39:05 +0100

    Update Card.kt

--- app/src/main/java/com/poussiere/hellokotlin/data/Card.kt
+++ app/src/main/java/com/poussiere/hellokotlin/data/Card.kt
@@ -4,4 +4,4 @@ package com.poussiere.hellokotlin.data
  * Created by poussiere on 24/01/18.
  */
 
-data class Card (val id : Int, val song : Int,  val discovered = false)
+data class Card (val id : Int, val song : Int,  val discovered : Boolean = false)
