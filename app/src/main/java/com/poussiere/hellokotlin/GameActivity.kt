9757b9d40ac2bdc69be6e92c010b6b6600766cfc
poussiere <von.linnasta@gmail.com>
2018-02-01 23:15:33 +0100

    update

--- app/src/main/java/com/poussiere/hellokotlin/GameActivity.kt
+++ app/src/main/java/com/poussiere/hellokotlin/GameActivity.kt
@@ -1,6 +1,8 @@
 package com.poussiere.hellokotlin
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
 import android.util.Log
@@ -32,6 +34,8 @@ class GameActivity : AppCompatActivity(), MyRecyclerViewAdapter.AdapterOnClickHa
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_game)
+
+
 
         //And we shuffle the cards
         cardTab.shuffle();
@@ -162,12 +166,7 @@ class GameActivity : AppCompatActivity(), MyRecyclerViewAdapter.AdapterOnClickHa
     }
 
     override fun onResume(){
-        game_board.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
         super.onResume()
+
     }
 }
--- app/src/main/java/com/poussiere/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/poussiere/hellokotlin/MainActivity.kt
@@ -2,14 +2,22 @@ package com.poussiere.hellokotlin
 
 import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.support.constraint.ConstraintLayout
+import android.support.v4.app.ActivityOptionsCompat
+import android.support.v4.app.ActivityOptionsCompat.makeSceneTransitionAnimation
+import android.support.v4.view.ViewCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
 import com.poussiere.hellokotlin.data.Card
 import com.poussiere.hellokotlin.utils.CardUtils
 import kotlinx.android.synthetic.main.activity_game.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.recycler_element.view.*
 import kotlin.collections.*
 
 
@@ -22,15 +30,17 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
         main_text_view.setText(R.string.main_text_view_content)
+
         
         main_text_view.setOnClickListener {
             val intent = Intent(this@MainActivity, GameActivity::class.java)
             player_nb.setText("")
-            ActivityOptionsCompat options = ActivityOptionsCompat.  
-                       makeSceneTransitionAnimation(this@MainActivity,
+            var options : ActivityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(this@MainActivity,
                          main_text_view,
                          ViewCompat.getTransitionName(main_text_view));
-                        startActivity(intent, options.toBundle());  
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                startActivity(intent, options.toBundle())
+            };  else startActivity(intent)
             
             
         
@@ -60,13 +70,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onResume() {
-        conteneur.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
-        super.onResume()
         player_nb.setText(R.string.un_joueur)
+        super.onResume()
+
     }
+
 }
